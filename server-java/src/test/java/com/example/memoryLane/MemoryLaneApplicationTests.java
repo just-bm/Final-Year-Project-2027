fa6b9ef738cd18cc0b26b45ec05c3ca35e5d1eb8
just-bm <tbalancemurugan@gmail.com>
2026-05-29 23:51:26 +0530

    project setup

---
+++ server-java/src/test/java/com/example/memoryLane/MemoryLaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoryLane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryLaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
